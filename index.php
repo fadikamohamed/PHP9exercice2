6e9ed9d4265175cdbdc79a4851a6f8b458f65a8a
fadikamohamed <fadika.mohamed02@gmail.com>
2018-09-03 15:23:02 +0200

    exercice terminé

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css" />
+        <title>Exercice 2</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row justify-content-center bg-dark text-white">
+                <div class="col-md-12">
+                    <p>
+                        <?php
+                        echo date('j-m-y');
+                        ?>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
